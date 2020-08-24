315179932c7cc2ce84965778a087180230fdd9d1
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-25 02:25:33 +0700

    Remove log MONGODB_URI

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 const MONGODB_URI =
   process.env.EXPRESS_APP_MONGODB_URI || 'mongodb://localhost:27017/template'
 
-console.info('MONGODB_URI:', MONGODB_URI)
-
 mongoose.set('useCreateIndex', true)
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
