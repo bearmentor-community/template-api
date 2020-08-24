fa25f826a55960de96d3c3aada13c6e73e4aad1a
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-25 03:05:25 +0700

    Use EXPRESS_APP_API_URL for baseUrl in seed

--- src/routes/users/middlewares/seed.js
+++ src/routes/users/middlewares/seed.js
@@ -11,16 +11,13 @@ module.exports = async (req, res) => {
       message: 'Seed initial users failed because they are already registered'
     })
   } else {
-    // Prepare base URL just for creating avatar URL
-    const baseUrl = req.protocol + '://' + req.get('host')
-
     // Use forEach, not insert, because have to encrypt password
     await seedUsersData.forEach(async (user) => {
       const { salt, hash } = await bcrypt.hashPassword(user.password)
 
       await User.create({
         ...user,
-        avatarUrl: `${baseUrl}/${user.avatarUrl}`,
+        avatarUrl: `${process.env.EXPRESS_APP_API_URL}/${user.avatarUrl}`,
         salt: salt,
         hash: hash
       })
