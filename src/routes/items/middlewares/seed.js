8fe146e9ff48633ce93a70aff45e2e4dc75473f0
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-27 21:51:17 +0700

    Update seed items

--- src/routes/items/middlewares/seed.js
+++ src/routes/items/middlewares/seed.js
@@ -7,13 +7,18 @@ module.exports = async (req, res) => {
   // Don't continue if the items already exists
   if (foundItem.length > 0) {
     res.status(400).send({
-      message: 'Failed to seed initial items because they are already exists'
+      message: 'Seed initial items failed because already exists'
     })
   } else {
-    await Item.insertMany(seedItemsData)
-
-    res.status(200).send({
-      message: 'Seed initial items completed'
-    })
+    try {
+      await Item.insertMany(seedItemsData)
+      res.status(200).send({
+        message: 'Seed initial items completed'
+      })
+    } catch (error) {
+      res.status(500).send({
+        message: 'Seed initial items failed because incorrect data'
+      })
+    }
   }
 }
